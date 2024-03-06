721a30d1b842b0743e6e08eef04e5a4d20fcef98
mdaiyub <aiyubali15-13456@diu.edu.bd>
2024-03-06 14:00:54 +0600

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from ChestDiagnosisClassifier import logger
+from ChestDiagnosisClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Chest Diagnosis Classifier!")
+#logger.info("Welcome to Chest Diagnosis Classifier!")
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
