ea51eafe7d93c94c84c3be23d4ddcdd1f779826b
mdaiyub <aiyubali15-13456@diu.edu.bd>
2024-03-06 18:55:43 +0600

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ChestDiagnosisClassifier import logger
 from ChestDiagnosisClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
+from ChestDiagnosisClassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 #logger.info("Welcome to Chest Diagnosis Classifier!")
 
@@ -16,3 +17,13 @@ except Exception as e:
     raise e
 
 
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
