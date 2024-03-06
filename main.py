3774466439fa5469d5168d555b842c2544af5136
mdaiyub <aiyubali15-13456@diu.edu.bd>
2024-03-06 20:05:53 +0600

    model trainer added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ChestDiagnosisClassifier import logger
 from ChestDiagnosisClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from ChestDiagnosisClassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ChestDiagnosisClassifier.pipeline.stage_3_model_trainer import ModelTrainingPipeline
 
 #logger.info("Welcome to Chest Diagnosis Classifier!")
 
@@ -18,11 +19,25 @@ except Exception as e:
 
 
 STAGE_NAME = "Prepare base model"
+
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
