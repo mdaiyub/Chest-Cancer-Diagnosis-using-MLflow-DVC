790b3336113c0a05223ca10e8f63b804af810d85
mdaiyub <aiyubali15-13456@diu.edu.bd>
2024-03-07 17:31:57 +0600

    mlflow added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ChestDiagnosisClassifier import logger
 from ChestDiagnosisClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from ChestDiagnosisClassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
 from ChestDiagnosisClassifier.pipeline.stage_3_model_trainer import ModelTrainingPipeline
+from ChestDiagnosisClassifier.pipeline.stage_4_model_evaluation import EvaluationPipeline
 
 #logger.info("Welcome to Chest Diagnosis Classifier!")
 
@@ -39,6 +40,19 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/ChestDiagnosisClassifier/config/configuration.py
+++ src/ChestDiagnosisClassifier/config/configuration.py
@@ -1,9 +1,10 @@
 import os
 from ChestDiagnosisClassifier.constants import *
-from ChestDiagnosisClassifier.utils.common import read_yaml, create_directories
+from ChestDiagnosisClassifier.utils.common import read_yaml, create_directories, save_json
 from ChestDiagnosisClassifier.entity.config_entity import (DataIngestionConfig,
                                                            PrepareBaseModelConfig,
-                                                           TrainingConfig)
+                                                           TrainingConfig,
+                                                           EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -71,3 +72,17 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/chest data", 
+            mlflow_uri="https://dagshub.com/mdaiyub/Chest-Cancer-Diagnosis-using-MLflow-DVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+
+
+
